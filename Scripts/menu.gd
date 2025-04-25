260587c4850024dffaf0a8c21f70f383cb78c362
yZempX <maurizio.3rrico@gmail.com>
2025-04-25 14:33:04 +0200

    Setup menu and basic elements

---
+++ Scripts/menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+@onready var game_manager: Node = $"."
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_start_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/game_manager.tscn")
+
+func _on_quit_btn_pressed() -> void:
+	get_tree().quit()
